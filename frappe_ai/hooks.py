7a2e42c6576302e77d4434fc5934fc8b72caac42
zafar26 <qutubuddinzafar2616118@gmail.com>
2026-08-04 06:08:53 +0530

    Changed Publisher name

--- frappe_ai/hooks.py
+++ frappe_ai/hooks.py
@@ -1,8 +1,8 @@
 app_name = "frappe_ai"
 app_title = "Frappe AI"
-app_publisher = "Your Name"
+app_publisher = "Zafar"
 app_description = "Natural-language agent that proposes and (on approval) creates Frappe/ERPNext documents, via a local GGUF model."
-app_email = "you@example.com"
+app_email = "zafar.erp@gmail.com"
 app_license = "mit"
 
 # Fixtures ---------------------------------------------------------------
